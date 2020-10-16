7afae55d401811809a0f5ed0f1aa082409fdf295
“Your <iammaya@live.ie>
2020-10-16 11:52:46 +0100

    Student tests added.

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,7 +1,12 @@
 import org.junit.jupiter.api.Test;
+import java.time.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StudentTest {
+    Student s1 = new Student("Maya McDevitt",21, LocalDate.of(1999,6,16));
+
     @Test
-    void name() {
+    void usernameTest() {
+        assertEquals("Maya McDevitt21",s1.getUsername());
     }
 }
