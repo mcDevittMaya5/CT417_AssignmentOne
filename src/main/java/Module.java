e75fc4c51007c04771d749a52ca6fd3077ea1338
“Your <iammaya@live.ie>
2020-10-16 16:37:14 +0100

    initial functions added to Moule

--- src/main/java/Module.java
+++ src/main/java/Module.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class Module {
+    private String moduleName;
+    private String moduleID;
+    private final ArrayList<Student> students = new ArrayList<>();
+    private final ArrayList<Course> courses = new ArrayList<>();
+
+    public Module(String moduleName,String moduleID) {
+        this.moduleName = moduleName;
+        this.moduleID = moduleID;
+    }
+
+    public String getModuleName() {return moduleName;}
+    public void setModuleName(String name){moduleName=name;}
+
+    public String getModuleID(){return moduleID}
+    public void setModuleID(String id){moduleID = id;}
+
+
 }
